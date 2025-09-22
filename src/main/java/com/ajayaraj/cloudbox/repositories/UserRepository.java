cda67105cf55d6eb5d675d5cc531f43b97234f57
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-22 22:46:22 +0530

    Added user repository.

---
+++ src/main/java/com/ajayaraj/cloudbox/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ajayaraj.cloudbox.repositories;
+
+import com.ajayaraj.cloudbox.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
