a04906080be4026acaadb094a8f8302532a5dc6e
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-23 19:39:25 +0530

    Update to register endpoint.

--- src/main/java/com/ajayaraj/cloudbox/controller/AuthenticationController.java
+++ src/main/java/com/ajayaraj/cloudbox/controller/AuthenticationController.java
@@ -4,7 +4,6 @@ import com.ajayaraj.cloudbox.dto.RegisterRequest;
 import com.ajayaraj.cloudbox.model.User;
 import com.ajayaraj.cloudbox.service.AuthenticationService;
 import jakarta.validation.Valid;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/auth")
@@ -27,15 +24,7 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@Valid @RequestBody RegisterRequest registerRequest) {
-        User user = new User();
-        user.setEmailId(registerRequest.getEmailId());
-        user.setFirstName(registerRequest.getFirstName());
-        user.setLastName(registerRequest.getLastName());
-        user.setPasswordHash(registerRequest.getPassword());
-        user.setCreatedAt(new Date());
-        user.setUpdatedAt(new Date());
-
-        return ResponseEntity.status(HttpStatus.OK).body(authenticationService.register(user));
+        return ResponseEntity.status(HttpStatus.OK).body(authenticationService.register(registerRequest));
     }
 
 }
--- src/main/java/com/ajayaraj/cloudbox/service/AuthenticationService.java
+++ src/main/java/com/ajayaraj/cloudbox/service/AuthenticationService.java
@@ -1,8 +1,13 @@
 package com.ajayaraj.cloudbox.service;
 
+import com.ajayaraj.cloudbox.dto.RegisterRequest;
+import com.ajayaraj.cloudbox.exception.ConflictException;
 import com.ajayaraj.cloudbox.model.User;
 import com.ajayaraj.cloudbox.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class AuthenticationService {
@@ -13,8 +18,31 @@ public class AuthenticationService {
         this.userRepository = userRepository;
     }
 
-    public User register(User user) {
+    public User register(RegisterRequest registerRequest) {
+
+        if(isEmailTaken(registerRequest.getEmailId())) {
+            throw new ConflictException("Email id already exists!");
+        }
+
+        User user = new User();
+        user.setFirstName(registerRequest.getFirstName());
+        user.setLastName(registerRequest.getLastName());
+        user.setEmailId(registerRequest.getEmailId());
+        user.setPasswordHash(hashPassword(registerRequest.getPassword()));
+        user.setCreatedAt(new Date());
+        user.setUpdatedAt(new Date());
+
         return userRepository.save(user);
+    }
+
+    public boolean isEmailTaken(String emailId) {
+        User user = userRepository.findByEmailId(emailId);
+
+        return user != null;
+    }
+
+    public String hashPassword(String password) {
+        return new BCryptPasswordEncoder().encode(password);
     }
 
 }
